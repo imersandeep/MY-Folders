e5a0e7a2eed5937fc7a10ed14ad99c927863332d
Sandeep Yadav <er,sandeep777@gmail.com>
2024-01-17 12:42:54 +0530

    second git class

--- src/gitdemoPackage/FirstGitClass.java
+++ src/gitdemoPackage/FirstGitClass.java
@@ -4,7 +4,8 @@ public class FirstGitClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("First Git Program");
+System.out.println("First Git Program loop");
+System.out.println("Second Program Main");
 	}
 
 }
