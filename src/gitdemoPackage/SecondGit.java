06596931187cdda2030efbf713bb13282ffbc22d
Sandeep Yadav <er,sandeep777@gmail.com>
2024-01-17 12:44:59 +0530

    changes in second c

--- src/gitdemoPackage/SecondGit.java
+++ src/gitdemoPackage/SecondGit.java
@@ -5,6 +5,7 @@ public class SecondGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("help");
+		System.out.println("New Folder added");
 
 	}
 
